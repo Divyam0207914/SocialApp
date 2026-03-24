14e9083796fc01feaa37ce9dafb8821d91f980df
Divyam Saraf <divyamsaraf192@gmail.com>
2026-03-25 01:23:28 +0530

    Api change

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://socialapp-b0fh.onrender.com',
 });
 
 // Add a request interceptor to attach the JWT token
